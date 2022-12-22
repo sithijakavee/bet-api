5e90cc37e2c0ca672a8a339993a03fec87d805b1
sithija <sithijakavee120@gmail.com>
2022-12-22 07:55:54 +0530

    datavase cresentials

--- database.py
+++ database.py
@@ -1,8 +1,8 @@
 import mysql.connector
 
 db = mysql.connector.connect(
-  host="localhost",
-  user="root",
-  password="",
+  host="banana.cnrmem3900ov.ap-south-1.rds.amazonaws.com",
+  user="admin",
+  password="20030609s",
   database="bet_db"
 )
